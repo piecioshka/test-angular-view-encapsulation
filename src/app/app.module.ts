77e8cd1dbbf441a7e408141cf10fba3dee12785e
Piotr Kowalski <piecioshka@gmail.com>
2019-01-06 19:34:58 +0100

    feat(components): Create four components with difference encapsulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmulatedComponent } from './components/emulated/emulated.component';
+import { NativeComponent } from './components/native/native.component';
+import { NoneComponent } from './components/none/none.component';
+import { ShadowDomComponent } from './components/shadow-dom/shadow-dom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmulatedComponent,
+    NativeComponent,
+    NoneComponent,
+    ShadowDomComponent
   ],
   imports: [
     BrowserModule
